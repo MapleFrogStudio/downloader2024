a4ae6492e2382ebb978c03b12a9d64e917c4c258
PoivronJaune <poivronjaune@gmail.com>
2023-12-28 00:32:43 -0500

    New downloader, manage bad symbols

---
+++ src/__init__.py
@@ -0,0 +1,42 @@
+from src import downloader as d
+
+def tsx():
+    print(f'__init__ -> Script ran tsx() function')
+    d.main('tsx')
+
+def nasdaq_nosector():
+    print(f'Script ran nasdaq_nosector() function')
+
+def nasdaq_cd():
+    print(f'Script ran nasdaq_cd() function')
+
+def nasdaq_fs():
+    print(f'Script ran nasdaq_fs() function')
+
+def nasdaq_healthcare():
+    print(f'Script ran nasdaq_healthcare() function')
+
+def nasdaq_industrials():
+    print(f'Script ran nasdaq_industrials() function')
+
+def nasdaq_technology():
+    print(f'Script ran nasdaq_technology() function')
+
+def nasdaq_cc():
+    print(f'Script ran nasdaq_nasdaq() function')
+
+def nasdaq_bm():
+    print(f'Script ran nasdaq_bm() function')
+
+def nasdaq_utilities():
+    print(f'Script ran nasdaq_utilities() function')
+
+def nasdaq_cs():
+    print(f'Script ran nasdaq_cs() function')
+
+def nasdaq_re():
+    print(f'Script ran nasdaq_re() function')
+
+def nasdaq_energy():
+    print(f'Script ran nasdaq_energy() function')    
+    
